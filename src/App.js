000af9f639da9fc3108e91d65f817174a15c640b
VivianTang11 <vivian.tang@hotmail.se>
2022-03-25 14:58:14 +0100

    removed component Task

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ export default function App() {
         <main>
           <Input />
           <Filter />
+          
           <TaskList />
         </main>
       </>
--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import Task from './Task'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../App';
 
 export default function TaskList() {
+  
+  const ctx = useContext(GlobalContext)
+
   return (
     <section className="task-section">
         <h2>Your Tasks</h2>
-        <Task />
-        <Task />
+        {ctx.tasks.map((task, index) => (
+          <article className='task' key={index}>
+            <input type="checkbox" />
+            <p>{task.text}</p>
+          </article>
+          ))
+        }
     </section>
   )
 }
