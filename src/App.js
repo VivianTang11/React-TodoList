fd831feec5705d369c99b286b42e52ea6549f578
VivianTang11 <vivian.tang@hotmail.se>
2022-03-25 10:28:12 +0100

    basic structuring in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <header>
+        <h1>ToDo-List</h1>
+      </header>
+      <main>
+        <section className="search-section">
+          <input type="text" placeholder="Add new task here..."></input>
+        </section>
+        <sction className="filter-section">
+          <h3>All</h3>
+          <h3>Uncompleted</h3>
+          <h3>Completed</h3>
+        </sction>
+        <section className="task-section">
+          <h2>Your Tasks</h2>
+        </section>
+      </main>
+    </>
   );
 }
 
